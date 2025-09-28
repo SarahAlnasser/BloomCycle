4eb4ef5e659f7976bb93e76bd6225d0a11b3aca4
Sarah AlNasser <sarahalnasser04@gmail.com>
2025-09-28 15:11:21 +0300

    Initial Commit

---
+++ BloomCycle/BloomCycleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomCycleApp.swift
+//  BloomCycle
+//
+//  Created by Sarah Alnasser on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct BloomCycleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
